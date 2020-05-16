ad776e0b7883797b398ab964ef48ab0eaf361b39
Hurricane0 <nikitadv777@gmail.com>
2020-05-16 22:57:40 +0300

    webpack config corrected

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require("path");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
-  entry: "index.js",
+  entry: "./index.js",
   mode: "development",
   output: {
     filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
