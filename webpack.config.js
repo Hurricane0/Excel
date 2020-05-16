61ffe533b228a80f57f0aa3abc1b5ffbbd882f1a
Hurricane0 <nikitadv777@gmail.com>
2020-05-16 22:56:54 +0300

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: "index.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+  },
+};
