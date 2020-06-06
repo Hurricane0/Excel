b40a36e32bc2625eeaac9ab5fada71e910b6ae58
Nikita <nikitadv777@gmail.com>
2020-06-06 22:22:24 +0300

    Handling components

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -4,10 +4,19 @@ export class Excel {
     this.components = options.components || [];
   }
 
+  getRoot() {
+    const $root = document.createElement('div');
+    this.components.forEach(Component => {
+      const component = new Component();
+      $root.insertAdjacentHTML('beforeend', component.toHTML());
+    });
+    return $root;
+  }
+
   render() {
     // this.$el.insertAdjacentHTML('afterbegin', '<h1>hello</h1>');
-    const node = document.createElement('h1');
-    node.textContent = 'Excel';
-    this.$el.append(node);
+    // const node = document.createElement('h1');
+    // node.textContent = 'Excel';
+    this.$el.append(this.getRoot());
   }
 }
