a4345f88b90631d587bc8151d8391a6f43a811dc
Nikita <nikitadv777@gmail.com>
2020-07-08 20:02:36 +0300

    Created root reducer

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import { Formula } from './components/formula/Formula';
 import { Header } from './components/header/Header';
 import { Toolbar } from './components/toolbar/Toolbar';
 import { Table } from './components/table/Table';
+import { createStore } from './core/createStore';
+import { rootReducer } from './redux/rootReducer';
+
+const store = createStore(rootReducer);
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
+  store,
 });
 
 excel.render();
