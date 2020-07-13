e898093b1922dc7ba3b4f5cb01500348bbd38235
Nikita <nikitadv777@gmail.com>
2020-07-13 20:24:29 +0300

    Debounce for table title

--- src/core/utils.js
+++ src/core/utils.js
@@ -77,7 +77,8 @@ export function debounce(fn, wait) {
   return function (...args) {
     const later = () => {
       clearTimeout(timeout);
-      fn(...args);
+      // eslint-disable-next-line
+      fn.apply(this, args);
     };
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
