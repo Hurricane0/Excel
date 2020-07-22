dd6793cb553a36e58072a1fedd4fd8fa4d720a2c
Nikita <nikitadv777@gmail.com>
2020-07-22 10:00:19 +0300

    Added basic unit tests for createStore function

--- src/core/store/createStore.test.js
+++ src/core/store/createStore.test.js
@@ -1,8 +1,18 @@
 import { createStore } from './createStore';
 
-describe('Test', () => {
-  test('Should Create Store:', () => {
-    const store = createStore(() => {}, {});
+const reducer = (state = 0, action) => {
+  if (action.type === 'ADD') {
+    return ++state;
+  }
+  return state;
+};
+
+describe('Create Store:', () => {
+  test('should return store object', () => {
+    const store = createStore(reducer);
     expect(store).toBeDefined();
+    expect(store.dispatch).toBeDefined();
+    expect(store.subscribe).toBeDefined();
+    expect(store.getState).toBeDefined();
   });
 });
